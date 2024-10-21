a3b586c3eca1d5b4df0bb43c0cfc1918c2226c0c
TSalud15 <saludtravis15@gmail.com>
2024-10-21 13:31:02 -0700

    Added font, icons and started styling main content

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,9 @@ module.exports = {
   content: ["./*.{html,js}"],
   theme: {
     extend: {
-      
+      fontFamily: {
+        poppins: ['Poppins', 'sans-serif']
+      }
     },
   },
   plugins: [],
