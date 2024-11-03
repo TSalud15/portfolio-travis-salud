78cec21517ad21b2eb4e4454a73f598e7e6f6a58
TSalud15 <saludtravis15@gmail.com>
2024-11-02 23:02:02 -0700

    Fixed navbar bug and added hero component

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,17 @@
-import Navbar from "./components/Navbar"
+import Navbar from "./components/Navbar";
+import Hero from "./components/Hero";
 
 const App = () => {
-  return (
-    <div>
-      <div className="absolute top-0 z-[-2] h-screen w-screen bg-neutral-950 bg-[radial-gradient(ellipse_80%_80%_at_50%_-20%,rgba(120,119,198,0.3),rgba(255,255,255,0))]"></div>
-      <header>
-        <Navbar />
-      </header>
-      
-    </div>
-  )
-}
-export default App
+    return (
+        <div>
+            <div className="absolute top-0 z-[-2] h-screen w-screen bg-neutral-950 bg-[radial-gradient(ellipse_80%_80%_at_50%_-20%,rgba(120,119,198,0.3),rgba(255,255,255,0))]"></div>
+            <header>
+                <Navbar />
+            </header>
+            <main>
+                <Hero />
+            </main>
+        </div>
+    );
+};
+export default App;
