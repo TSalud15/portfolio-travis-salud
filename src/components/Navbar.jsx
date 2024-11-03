45a3e45357f8a9ce45d1f1bcfb93cdcb288d9b12
TSalud15 <saludtravis15@gmail.com>
2024-11-02 22:34:37 -0700

    Implemented responsive design for navbar component

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,41 +1,72 @@
+import { FaBarsStaggered } from "react-icons/fa6";
+import { FaTimes } from "react-icons/fa";
+import { useState } from "react";
 
 const Navbar = () => {
-    const links = [
-        {
-            id: 1,
-            name: "About"
-        },
-        {
-            id: 2,
-            name: "Projects"
-        },
-        {
-            id: 3,
-            name: "Skills"
-        },
-        {
-            id: 4,
-            name: "Contact"
-        },
-    ]
+  const [menuOpen, setMenuOpen] = useState(false);
 
-    return (
-        <nav className="flex justify-between items-center pt-6 px-4 text-white w-full">
-            <div>
-                <h1 className="text-2xl">Travis Salud</h1>
-            </div>
-            <ul className="flex">
-                {links.map(link => {
-                    return (
-                    <li 
-                    key={link.id}
-                    className="px-4 text-slate-300 hover:scale-110 hover:text-white duration-200"
-                    >
-                        <a href="#">{link.name}</a>
-                    </li>
-                )})}
-            </ul>
-        </nav>
-    )
-}
-export default Navbar
+  const links = [
+    {
+      id: 1,
+      link: "about",
+    },
+    {
+      id: 2,
+      link: "projects",
+    },
+    {
+      id: 3,
+      link: "skills",
+    },
+    {
+      id: 4,
+      link: "contact",
+    },
+  ];
+
+  return (
+    <nav className="relative flex justify-between items-center pt-6 px-4 text-white w-full">
+      <div>
+        <h1 className="text-2xl">Travis Salud</h1>
+      </div>
+      <ul className="hidden md:flex">
+        {links.map(({ id, link }) => {
+          return (
+            <li
+              key={id}
+              className="px-4 capitalize text-slate-300 hover:scale-110 hover:text-white duration-200"
+            >
+              <a href="#">{link}</a>
+            </li>
+          );
+        })}
+      </ul>
+
+      <div
+        className="cursor-pointer pr-4 z-10 text-slate-300 hover:text-white duration-200 md:hidden"
+        onClick={() => setMenuOpen(!menuOpen)}
+      >
+        {menuOpen ? <FaTimes size={25} /> : <FaBarsStaggered size={25} />}
+      </div>
+
+      <ul
+        className={`absolute md:hidden top-14 left-0 w-full backdrop-blur flex flex-col items-center gap-6 font-semibold text-lg transform transition-transform ${
+          menuOpen ? "opacity-100" : "opacity-0"
+        }`}
+        style={{ transition: "transform 0.3s ease, opacity 0.3s ease" }}
+      >
+        {links.map(({ id, link }) => {
+          return (
+            <li
+              key={id}
+              className="w-full p-4 capitalize text-center text-slate-300 cursor-pointer hover:text-white duration-200"
+            >
+              <a href="#">{link}</a>
+            </li>
+          );
+        })}
+      </ul>
+    </nav>
+  );
+};
+export default Navbar;
